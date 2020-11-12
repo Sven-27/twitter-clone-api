3adae5f6574caaecf90f3ca1b949d79a0da4ea20
Sven Notermans <nootje27@outlook.com>
2020-11-12 15:53:26 +0100

    added env file

---
+++ exercise/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const Twitter = require('./api/helpers/twitter.js')
+twitter = new Twitter();
+const app = express()
+const port = 3000
+require('dotenv').config()
+
+app.use((req, res, next) =>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+})
+
+app.get('/tweets', (req, res) => {
+  const query = req.query.q;
+  const count = req.query.count;
+  console.log(process.env.TWITTER_API_TOKEN)
+  twitter.get(query, count).then((response) => {
+    res.status(200).send(response.data)
+  }).catch((error)=> {
+    res.status(400).send(error)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Twitter api listening at http://localhost:${port}`)
+})
