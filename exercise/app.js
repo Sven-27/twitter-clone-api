8693560f61efb98d0460891f87da82e85ab246b7
Sven Notermans <nootje27@outlook.com>
2020-11-18 19:22:43 +0100

    completed to deploy

--- exercise/app.js
+++ exercise/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Twitter = require('./api/helpers/twitter.js')
 twitter = new Twitter();
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 require('dotenv').config()
 
 app.use((req, res, next) =>{
